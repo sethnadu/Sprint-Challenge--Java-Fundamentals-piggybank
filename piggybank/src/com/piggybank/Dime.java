ffefb57dae95ef8056f1527892db2f4ab0ff8440
Seth Nadu <seth.nadu@gmail.com>
2019-09-06 12:31:18 -0400

    MVP done attempting stretch

---
+++ piggybank/src/com/piggybank/Dime.java
@@ -0,0 +1,16 @@
+package com.piggybank;
+
+public class Dime extends AbstractCoins
+{
+    public Dime(int number)
+    {
+        super(.10, number, "Dimes");
+    }
+    public Dime()
+    {
+        super(.10, 1, "Dime");
+    }
+
+
+
+}
